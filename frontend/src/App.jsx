d44384761745f1ea436b330bacb218ec01f9cd9a
kaname140 <s2622066@stu.musashino-u.ac.jp>
2026-08-05 04:31:42 +0900

    feat: implement sidebar component with toggle functionality and keyboard accessibility

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar'
 import StatusBar from './components/StatusBar/StatusBar'
 import Home from './pages/Home/Home'
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((isOpen) => !isOpen)
+  }
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onMenuClick={toggleSidebar} isMenuOpen={isSidebarOpen} />
+      <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
       <StatusBar />
       <Home />
     </>
